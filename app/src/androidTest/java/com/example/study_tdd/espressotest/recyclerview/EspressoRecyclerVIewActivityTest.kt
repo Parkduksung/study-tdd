b6e808535c74377d959d85e05ba704c222440e1c
Parkduksung <duksung1234@naver.com>
2021-02-24 23:58:25 +0900

    [Espresso recyclerView Test] 테스트 클래스 생성.

---
+++ app/src/androidTest/java/com/example/study_tdd/espressotest/recyclerview/EspressoRecyclerVIewActivityTest.kt
@@ -0,0 +1,11 @@
+package com.example.study_tdd.espressotest.recyclerview
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class EspressoRecyclerVIewActivityTest {
+
+
+}
