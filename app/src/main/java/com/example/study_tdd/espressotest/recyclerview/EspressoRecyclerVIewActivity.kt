8dc8c92310b44ba6825ebe9a37ff146d2801e909
Parkduksung <duksung1234@naver.com>
2021-02-24 23:08:28 +0900

    [Espresso recyclerView Test] 기본 설정.

---
+++ app/src/main/java/com/example/study_tdd/espressotest/recyclerview/EspressoRecyclerVIewActivity.kt
@@ -0,0 +1,14 @@
+package com.example.study_tdd.espressotest.recyclerview
+
+import android.os.Bundle
+import com.example.study_tdd.R
+import com.example.study_tdd.base.BaseActivity
+import com.example.study_tdd.databinding.ActivityEspressoTestBinding
+
+class EspressoRecyclerVIewActivity :
+    BaseActivity<ActivityEspressoTestBinding>(R.layout.activity_espresso_recycler) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
