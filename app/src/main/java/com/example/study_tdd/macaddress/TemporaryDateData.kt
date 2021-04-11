ccf5d52d69ab315eba9c9d33e7b338a881d0c481
Parkduksung <duksung1234@naver.com>
2021-04-11 21:29:26 +0900

    [대체맥어드레스]일시적 날짜 데이터 작업.

--- app/src/main/java/com/example/study_tdd/macaddress/TemporaryDateData.kt
+++ app/src/main/java/com/example/study_tdd/macaddress/TemporaryDateData.kt
@@ -1,4 +1,19 @@
 package com.example.study_tdd.macaddress
 
-class TemporaryDateData {
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+class TemporaryDateData : AlternativeMacAddress {
+
+    private var _address = ""
+
+    override val address: String
+        get() = _address
+
+    @SuppressLint("SimpleDateFormat")
+    override fun getAddress(): Boolean {
+        _address = SimpleDateFormat("yyMMddHHmmss").format(Date());
+        return true
+    }
 }
