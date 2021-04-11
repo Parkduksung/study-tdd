3e3babe97bbd083b4f55506c61a427764a6b9296
Parkduksung <duksung1234@naver.com>
2021-04-11 21:39:19 +0900

    [대체맥어드레스]SSAID 획득 작업.

--- app/src/main/java/com/example/study_tdd/macaddress/SSAId.kt
+++ app/src/main/java/com/example/study_tdd/macaddress/SSAId.kt
@@ -1,4 +1,32 @@
 package com.example.study_tdd.macaddress
 
-class SSAId {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
+
+class SSAId(private val context: Context) : AlternativeMacAddress {
+
+    private var _address = ""
+
+    override val address: String
+        get() = _address
+
+    @SuppressLint("HardwareIds")
+    override fun getAddress(): Boolean {
+        return try {
+            val getSSAId =
+                Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+
+            if (getSSAId != null) {
+                _address = getSSAId
+                true
+            } else {
+                _address = ""
+                false
+            }
+        } catch (e: Exception) {
+            _address = ""
+            false
+        }
+    }
 }
